4225e44a21fd67ff91c509cc99360fe1dc663c04
Mert <mertarslan260@gmail.com>
2024-12-23 23:23:29 +0300

    SQL Çalışması

---
+++ Bireysel/GroupBy2.sql
@@ -0,0 +1,10 @@
+SELECT
+	model_year,
+	MIN(list_price) AS [Min Price],
+	MAX(list_price) AS [Max Price],
+	COUNT(list_price)  AS [Row Count],
+	SUM(list_price)  AS [Total Price],
+	AVG(list_price)  AS [Average Price]
+FROM production.products
+GROUP BY model_year
+ORDER BY SUM(list_price) DESC
